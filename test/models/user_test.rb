7835a0cc9c0e81721bb281ca467e1a40bf69fb73
gevorkia <liligevorkian@gmail.com>
2020-10-12 19:27:06 -0700

    Add remaining files updated from configuration and other

---
+++ test/models/user_test.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  about_me        :text
+#  location        :string
+#  privacy         :boolean          default(FALSE)
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
